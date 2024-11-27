22caf504eae835807319d09cbc2b5e898de6edfd
Soykot Hosen <soykothosen1603028@gmail.com>
2024-11-27 19:42:49 +0600

    Upto Class  27 Nov Class

---
+++ 7.ModulePythonBasics–OOPandDataStructures/Practise/RUETJS/myproject.py
@@ -0,0 +1,17 @@
+#import ruetjs as rj
+
+
+#button1 = rj.Button('Green',"Circular")
+#link1 = rj.Link("Blue")
+
+#link1.Click("New Tab")
+#print(button1.Color)
+
+from ..ruetJsNew import Button
+
+button1 = Button('Green',"Circular")
+print(button1.Color)
+
+#Another Folder Module
+#Python Fixed Variable Type
+
